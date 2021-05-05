0c9db619eb60599fb0ea9f0ed6db5c05d810d941
devil1321 <s.dominik1321@gmail.com>
2021-05-05 19:44:26 +0200

    fixed draggable list only animation to fix

--- src/components/DragAnswer.js
+++ src/components/DragAnswer.js
@@ -1,19 +1,43 @@
 import React,{useContext,useEffect,useState} from 'react'
+import { useLocation } from 'react-router-dom'
 import {DataContext} from '../api/context'
 import { Draggable } from 'react-beautiful-dnd'
 const DragAnswer = ({index,text,theme}) => {
-    const {comesIn,isBack} = useContext(DataContext)
+    const {comesIn,isBack,answers} = useContext(DataContext)
     const [load,setLoad] = useState(false)
- 
+    const [size,setSize] = useState(window.innerWidth)
+    const location = useLocation()
+    let path = location.pathname
     useEffect(()=>{
-        let sheets = document.styleSheets;
-        let selector = ".draggable__btn"
+        window.addEventListener('resize',()=>{setSize(window.innerWidth)})
+        let btns = document.querySelectorAll('.draggable__btn')
+        if(path === `/${theme}/list-drag-and-drop`){
+            if(size < 768){
+                btns.forEach(btn=>{
+                    btn.style.position = 'relative'
+                    btn.style.bottom = '0px'
+                    btn.style.left = '-5%'
+                    btn.style.margin = '0px 15px !important'
+                    btn.style.width = '250px'
+                })
+            }else if(size >= 768){
+                btns.forEach(btn=>{
+                    btn.style.position = 'relative'
+                    btn.style.bottom = '-400px'
+                    btn.style.left = '0%'
+                    btn.style.margin = '0px'
+                    btn.style.width = '200px'
+                })
+            }
+        }
         if(load === false && !isBack){
             comesIn()
             setLoad(true)
         }
         else{
             setTimeout(()=>{
+                let sheets = document.styleSheets;
+                let selector = ".draggable__btn"
                 for (let sheet of sheets) {
                     for (let rule of sheet.cssRules) {
                     if (rule.selectorText === selector) {
@@ -26,7 +50,7 @@ const DragAnswer = ({index,text,theme}) => {
             },2000)
         }
        
-    },[load,index])
+    },[load,index,size,answers])
     return (
         <Draggable  draggableId={text.toString()} key={index} index={index}>
         {(provided)=>(
--- src/pages/ListDragAndDrop/index.js
+++ src/pages/ListDragAndDrop/index.js
@@ -2,7 +2,6 @@ import React,{useState ,useEffect,useContext} from 'react'
 import { useHistory,useLocation } from 'react-router-dom'
 import {DataContext} from '../../api/context'
 import Header from '../../components/Header'
-import DropPlace from '../../components/DropPlace'
 import DragAnswer from '../../components/DragAnswer'
 import Question from '../../components/Question'
 import { DragDropContext, Droppable } from 'react-beautiful-dnd'
@@ -12,21 +11,18 @@ const ListDragAndDrop = ({theme}) => {
     const  browserHistory = useHistory()
     const [isCorrect,setIsCorrect] = useState(false)
     const [isFalse,setIsFalse] = useState(false)
-    
     let location = useLocation()
     let path = location.pathname
     useEffect(()=>{
         setScss(theme,path)
         setIsFalse(false)
         setIsCorrect(false)
-        
         if(isLoad !== true && !isBack && !isCheck && listIndex < 2){          
             setQuestionsDragAndCheck(list,setQuestion,setAnswers,setCorrect,listIndex,theme)
         }else if(isCheck && isBack && listIndex < 2){
             setQuestionsDragAndCheck(list,setQuestion,setAnswers,setCorrect,listIndex,theme)
             questionIn()
         }
-        
         if(listIndex == 0){
             setIndex(-1)
         }
@@ -34,7 +30,6 @@ const ListDragAndDrop = ({theme}) => {
         if(listIndex == 1){
             setIndex(1)
         }
-        console.log(index)
         let tempLocation = [...locationHistory]
         tempLocation[listIndex] = path
         setLocationHistory(tempLocation)
@@ -58,12 +53,11 @@ const ListDragAndDrop = ({theme}) => {
             setIsFalse(true)
         }
         setThemeURL(theme)
-        if(reload < 2) setReload(reload + 1)
-        
+        if(reload < 2) setReload(reload + 1)        
     },[index,listIndex,reload,isCheck,isCorrect,isFalse,question,isBack])
     // do contextu
     return (
-        <div className={`draggable ${theme}`} >
+        <div className={`draggable ${theme} list`} >
             <Header theme={theme} subtitle="DRAG & DROP THE RIGHT ANSWER" current={listIndex+1} max="2"/>
             <Question question={question} nr={`${listIndex+1}. `}/>
             <div className="draggable__action">
@@ -73,7 +67,7 @@ const ListDragAndDrop = ({theme}) => {
                     {isFalse? <h2 className="draggable__answer">Wrong<div className="draggable__wrong-text"></div></h2> : null}
                     <div className="draggable__answers">
                         {answers.map((text,index)=> 
-                            <Droppable  key={text}  droppableId={`draggable__row-${index}`} direction="horizontal" >
+                            <Droppable  key={text}  droppableId={`draggable__row-${index}`} direction={"horizontal"} >
                                 {(provided)=>(
                                 <div className={`draggable__row-${index} draggable--row`}  {...provided.draggableProps} ref={provided.innerRef}>
                                     <DragAnswer correct={correct} index={index} text={text} theme={theme} />
